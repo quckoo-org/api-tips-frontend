d511e512d4c86ce922124c6b67235dec4f2608dd
kirill grohl <grohl@deeplay.io>
2024-12-03 11:49:20 +0300

    upd - theme config

--- config/tailwind/config.ts
+++ config/tailwind/config.ts
@@ -1,6 +1,6 @@
 import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config";
 import { TailwindThemeT } from "./types";
+import tailwindConfig from "../../tailwind.config";
 
 export const twConfig = resolveConfig(tailwindConfig);
 
