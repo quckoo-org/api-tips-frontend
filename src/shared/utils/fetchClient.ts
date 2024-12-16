77d5acf60f6a0710b3bca455c813abf5d4522beb
HOME-PC\Max <gmd29999@yandex.ru>
2024-12-16 10:32:39 +0300

    fix: address

--- src/shared/utils/fetchClient.ts
+++ src/shared/utils/fetchClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_GRPC_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
   withCredentials: true, // Для работы с куками
 });
