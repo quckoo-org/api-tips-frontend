5d27a5f2f354e80319f56411da176207b3d05918
HOME-PC\Max <gmd29999@yandex.ru>
2025-01-30 09:33:45 +0300

    fix: переводы

--- src/entities/requisites/index.ts
+++ src/entities/requisites/index.ts
@@ -1,7 +1,1 @@
-export type {
-  RequisitesT,
-  BankAccountRequisites,
-  CryptoWalletRequisites,
-} from "@/entities/requisites/model/types";
-
 export { useGetRequisites } from "@/entities/requisites/model/use-get-requisites";
