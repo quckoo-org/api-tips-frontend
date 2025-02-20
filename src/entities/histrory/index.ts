2b6799a017618ef0426a3a3f89db3a42b980d06a
kirill grohl <grohl@deeplay.io>
2025-02-20 11:00:14 +0300

    feat: [DP-1340] - fix build errors

--- src/entities/histrory/index.ts
+++ src/entities/histrory/index.ts
@@ -1,5 +1,4 @@
-export { useGetHistories } from "./model/use-get-histories";
-
-export { useGetHistoriesByUser } from "./model/use-get-hisrories-by-user";
-
-export { useGetDetailedHistories } from "./model/use-get-detailed-histories";
+export {
+  useGetHistories,
+  useGetDetailedHistories,
+} from "./model/use-get-histories";
--- src/features/history-filters/model/types.ts
+++
@@ -1,1 +0,0 @@
-export type OrdersFiltersT = any;
