725c616a4ae5ef132a41ea1b486037d2938f6397
kirillgileadd <kirillgilead@gmail.com>
2025-02-25 11:03:23 +0300

    feat: [DP-1590] - add invoice status

--- src/entities/invoices/index.ts
+++ src/entities/invoices/index.ts
@@ -2,3 +2,6 @@ export { useGetInvoices } from "./model/use-get-invoices";
 export { useInvoicesPaymentType } from "./model/use-invoices-payment-type";
 
 export { InvoicePaymentText } from "./ui/invoice-payment-text";
+export { InvoiceStatusText } from "./ui/invoice-status-text";
+
+export { useGetInvoiceStatus } from "./model/use-get-invoice-status";
--- src/features/manage-invoices/ui/update-invoice-form.tsx
+++ src/features/manage-invoices/ui/update-invoice-form.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import { Button, Flex, NumberInput, Text, TextInput } from "@mantine/core";
+import {
+  Button,
+  Flex,
+  NumberInput,
+  Select,
+  Text,
+  TextInput,
+} from "@mantine/core";
 import { clsx } from "clsx";
 import { FC } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { UpdateInvoiceFormValuesT } from "@/features/manage-invoices/model/types";
 import { useTranslations } from "@/shared/locale/translations";
 import { Invoice } from "@/shared/proto/api_tips_invoice/v1/api_tips_invoice";
+import { InvoiceStatus } from "@/shared/proto/custom_enums/v1/custom_enums";
 
 type InvoiceFormProps = {
   className?: string;
@@ -23,6 +31,8 @@ export const UpdateInvoiceForm: FC<InvoiceFormProps> = ({
   isLoading,
   error,
 }) => {
+  console.log(invoice, "invoice");
+
   const { t } = useTranslations();
   const {
     handleSubmit,
@@ -31,9 +41,30 @@ export const UpdateInvoiceForm: FC<InvoiceFormProps> = ({
     control,
   } = useForm<UpdateInvoiceFormValuesT>({
     defaultValues: {
-      ...invoice,
+      invoiceStatus: invoice?.status,
+      amountOfRequests: invoice?.amountOfRequests,
+      refNumber: invoice?.refNumber,
     },
   });
+
+  const STATUS_DATA = [
+    {
+      value: InvoiceStatus.INVOICE_STATUS_CANCELLED,
+      label: t("invoice_status_cancelled"),
+    },
+    {
+      value: InvoiceStatus.INVOICE_STATUS_CREATED,
+      label: t("invoice_status_created"),
+    },
+    {
+      value: InvoiceStatus.INVOICE_STATUS_PAID,
+      label: t("invoice_status_paid"),
+    },
+    {
+      value: InvoiceStatus.INVOICE_STATUS_UNSPECIFIED,
+      label: t("invoice_status_unspecified"),
+    },
+  ];
 
   const onSubmit = (data: UpdateInvoiceFormValuesT) => {
     onSuccess(data);
@@ -68,9 +99,22 @@ export const UpdateInvoiceForm: FC<InvoiceFormProps> = ({
             />
           )}
         />
-
+        <Controller
+          name="invoiceStatus"
+          control={control}
+          rules={{ required: t("invoice_status") }}
+          render={({ field }) => (
+            <Select
+              label={t("invoice_status")}
+              placeholder={t("enter_invoice_status")}
+              value={field.value}
+              onChange={field.onChange}
+              data={STATUS_DATA}
+            />
+          )}
+        />
         <Button loading={isLoading} type="submit">
-          Submit
+          {t("submit")}
         </Button>
       </Flex>
     </form>
