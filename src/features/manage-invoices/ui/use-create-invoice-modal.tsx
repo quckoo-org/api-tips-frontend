d37afa88b10b9487848f82acc1698fe22e1afc96
kirill grohl <grohl@deeplay.io>
2025-03-04 10:30:09 +0300

    feat: [DP-1667] - Rename the parameter Invoice to invoice

--- src/features/manage-invoices/ui/use-create-invoice-modal.tsx
+++ src/features/manage-invoices/ui/use-create-invoice-modal.tsx
@@ -5,7 +5,7 @@ import { Invoice } from "@/shared/proto/api_tips_invoice/v1/api_tips_invoice";
 export const useCreateInvoiceModal = () => {
   const [modalProps, setModalProps] = useState<{
     onSuccess?: () => void;
-    onClose: (Invoice?: Invoice) => void;
+    onClose: (invoice?: Invoice) => void;
     orderId?: number;
     isCustomer?: boolean;
   }>();
@@ -18,8 +18,8 @@ export const useCreateInvoiceModal = () => {
   ) => {
     return new Promise<Invoice | undefined>((res) => {
       setModalProps({
-        onClose: (Invoice) => {
-          res(Invoice);
+        onClose: (invoice) => {
+          res(invoice);
           setModalProps(undefined);
         },
         orderId,
